d4956e54952c8e7bf8b5c69c0aad610da04901d5
Ulises Martinez Araiza <mx.ulises@gmail.com>
2023-08-27 15:48:50 -0700

    Simulate long startup time for this app

--- my-app/app.py
+++ my-app/app.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from flask import Flask, jsonify
 
@@ -9,6 +10,8 @@ def hello():
     return jsonify({'msg': "Hello World!"})
 
 if __name__ == '__main__':
+    # Simulate a long startup time
+    time.sleep(60)
     # Initialize Flask App
     http_port = int(os.environ.get('HTTP_PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=http_port)
